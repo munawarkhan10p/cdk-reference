523b8630e8b28fcb7c8709613fcec63965e87652
munawarkhan10p <munawarkhan6656@gmail.com>
2021-01-23 17:34:53 +0500

    changes in modules import and file names

--- bin/cdk-reference.ts
+++ bin/cdk-reference.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkReferenceStack } from '../lib/cdk-reference-stack';
+import { CdkReferenceStack } from '../cdk/cdk-reference-stack';
 
 const app = new cdk.App();
 new CdkReferenceStack(app, 'CdkReferenceStack');
--- test/cdk-reference.test.ts
+++ test/cdk-reference.test.ts
@@ -1,9 +1,9 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as CdkReference from '../lib/cdk-reference-stack';
+import { App } from '@aws-cdk/core';
+import * as CdkReference from '../cdk/cdk-reference-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
     const stack = new CdkReference.CdkReferenceStack(app, 'MyTestStack');
     // THEN
